b34361d3cee4580363ec7088600f190f5a9cd5dd
Dave Tapley <dukedave@gmail.com>
2017-12-22 13:46:45 -0700

    Test opencv
    Installed OpenCV 3.3.1 by doing most of this:
    https://github.com/LumiGuide/haskell-opencv/blob/59208d5e32d9a99217124909431bbfd5ea4ab9f7/travis-build-opencv
    
    Then this:
    https://github.com/LumiGuide/haskell-opencv/blob/59208d5e32d9a99217124909431bbfd5ea4ab9f7/.travis.yml#L39-L41

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,45 @@
+{-# language DataKinds #-}
+{-# language FlexibleInstances #-}
+
 module Lib
     ( someFunc
     ) where
 
+import Control.Monad ( unless )
+import qualified OpenCV as CV
+import OpenCV.TypeLevel
+import OpenCV.VideoIO.Types
+
 someFunc :: IO ()
-someFunc = putStrLn "Derp derp"
+someFunc = do
+    cap <- CV.newVideoCapture
+    -- Open the first available video capture device. Usually the
+    -- webcam if run on a laptop.
+    CV.exceptErrorIO $ CV.videoCaptureOpen cap $ CV.VideoDeviceSource 1 Nothing
+    isOpened <- CV.videoCaptureIsOpened cap
+
+    case isOpened of
+      False -> putStrLn "Couldn't open video capture device"
+      True -> do
+         w <- CV.videoCaptureGetI cap VideoCapPropFrameWidth
+         h <- CV.videoCaptureGetI cap VideoCapPropFrameHeight
+
+         putStrLn $ "Video size: " ++ show w ++ " x " ++ show h
+
+         CV.withWindow "video" $ \window -> do
+                loop cap window
+  where
+    loop cap window = do
+      _ok <- CV.videoCaptureGrab cap
+      mbImg <- CV.videoCaptureRetrieve cap
+      case mbImg of
+        Just img -> do
+          -- Assert that the retrieved frame is 2-dimensional.
+          let img' :: CV.Mat ('S ['D, 'D]) 'D 'D
+              img' = CV.exceptError $ CV.coerceMat img
+          CV.imshow window img'
+          key <- CV.waitKey 20
+          -- Loop unless the escape key is pressed.
+          unless (key == 27) $ loop cap window
+        -- Out of frames, stop looping.
+        Nothing -> pure ()
