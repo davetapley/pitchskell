c76a0f2420e2345a4a06bff39865398abf851394
Dave Tapley <dukedave@gmail.com>
2018-05-06 01:19:20 -0700

    Render track path

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,32 @@
 module Main where
 
 import Lib
+import Loop
 import Track
 
-import qualified Gloss.Main as GM
+import qualified Graphics.Gloss as G
 
 main :: IO ()
---main = someFunc
+main
+ = G.display
+        (G.InWindow
+               "Hello World"     -- window title
+                (1000, 1000)     -- window size
+                (10, 10))        -- window position
+        G.black                  -- background color
+        picture                  -- picture to display
 
-main = GM.main
+
+testTrack = Track.parseTrack "srrsrr"
+
+scalePoint :: Point -> (Float, Float)
+scalePoint (Point x y) = (fromRational x * 100, fromRational y * 100)
+
+tilePath :: Segment -> G.Path
+tilePath (Segment _ entrance exit) = [scalePoint entrance, scalePoint exit]
+
+segmentPicture :: G.Color -> Segment -> G.Picture
+segmentPicture color = G.Color color . G.line . tilePath
+
+picture :: G.Picture
+picture = G.pictures $ zipWith segmentPicture (cycle [G.white, G.red]) (unfold testTrack)
